10c8c54030d1025f0cd692c844a892b0401a1650
Owenn Gimli <owenn2106@icloud.com>
2020-12-12 12:14:32 +0800

    product category page 1.0

--- products_app.js
+++ products_app.js
@@ -1,6 +1,6 @@
 const tl = gsap.timeline({defaults: {ease: "power1.out"}})
 
-tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1})
+tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 2})
 
 const navSlide = () => {
     const burger = document.querySelector('.burger')
